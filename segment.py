2c33e9ed09f71ce1858f0a7222ccb206fc45ba2a
Ophir Lifshitz <hangfromthefloor@gmail.com>
2014-03-02 04:14:14 -0400

    Current version

--- segment.py
+++ segment.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import collections
 import marisa_trie
 import string, datrie
@@ -16,26 +17,36 @@ class ReversedTrie:
     def __reverse_list(l):
         return map(ReversedTrie.__reverse_str, l)
 
-    def keys(self, prefix):
-        return ReversedTrie.__reverse_list(self.trie.keys(ReversedTrie.__reverse_str(prefix)))
+    @staticmethod
+    def __prefix_or_none(prefix):
+        return None if not prefix else ReversedTrie.__reverse_str(prefix)
 
-    def has_keys_with_prefix(self, prefix):
-        return self.trie.has_keys_with_prefix(ReversedTrie.__reverse_str(prefix))
+    def keys(self, prefix=None):
+        return ReversedTrie.__reverse_list(self.trie.keys(ReversedTrie.__prefix_or_none(prefix)))
+
+    def has_keys_with_prefix(self, prefix=None):
+        return self.trie.has_keys_with_prefix(ReversedTrie.__prefix_or_none(prefix))
+
+    def iter_prefix_items(self, prefix=None):
+        iter = self.trie.iter_prefix_items(ReversedTrie.__prefix_or_none(prefix))
+        for i in iter:
+            yield (lambda (k, v): (ReversedTrie.__reverse_str(k), v))(i)
 
     def save(self, f):
         return self.trie.save(f)
 
+    def __contains__(self, key):
+        return self.trie.__contains__(self.__reverse_str(key))
+
+    def __getitem__(self, key):
+        return self.trie.__getitem__(self.__reverse_str(key))
+
+    def __setitem__(self, key, value):
+        return self.trie.__setitem__(self.__reverse_str(key), value)
+
 
 BOLD_ON, BOLD_OFF, UNDERLINE_ON, UNDERLINE_OFF = '\033[1m', '\033[22m', '\033[4m', '\033[24m'
 COMMAND_ON, COMMAND_OFF, WORD_ON, WORD_OFF = BOLD_ON, BOLD_OFF, UNDERLINE_ON, UNDERLINE_OFF
-
-def memoize(f):
-    class memodict(dict):
-        __slots__ = ()
-        def __missing__(self, key):
-            self[key] = ret = f(key)
-            return ret
-    return memodict().__getitem__
 
 REWRITE = False
 FIXTURE = False
@@ -68,201 +79,117 @@ test_words = []
 [test_words.append(command[c:]) for command in commands for c in range(len(command)) if command[c:] not in test_words]
 #test_words = ['orange','tier', 'it']
 
-numbers = range(1, 9)
-
-memoized = {}
-commands_ending_with_memoized = {}
-words_ending_with_memoized = {}
+numbers = range(1, 1+9)
 
 def complete(this_word, n):
     for i in range(len(this_word), 0, -1):
         end_of_this_command = this_word[:i]
         end_of_this_word = this_word[1+i:]
-        print '\t'*n+'('+str(i)+'\t',BOLD_ON+end_of_this_command+BOLD_OFF
-        prev_commands = ending_with('command', end_of_this_command, 1+n)
-        print '\t'*n+')',str(prev_commands)
+        ##print '\t'*n+'('+str(i)+'\t',BOLD_ON+end_of_this_command+BOLD_OFF
+        prev_commands = ending_with(CommandWhich, end_of_this_command, 1+n)
+        ##print '\t'*n+')',str(prev_commands)
         if prev_commands:
             return prev_commands
+    
+def add_name_stuff(cls):
+    cls.reversed_trie = globals()['reversed_' + cls.name + 's_trie']
+    cls.on = globals()[cls.name.upper() + '_ON']
+    cls.off = globals()[cls.name.upper() + '_OFF']
+    return cls
 
 class Which:
-    def __init__(self, name):
-        self.name = name
-        self.reversed_trie = globals()['reversed_' + name + 's_trie']
-        self.on = globals()[key.upper() + '_ON']
-        self.off = globals()[key.upper() + '_OFF']
-        
-    memo = datrie.Trie(string.ascii_lowercase)
+    memo = ReversedTrie(datrie.Trie(string.ascii_lowercase))
+    memo[u'zzz'] = None # Because datrie hangs if empty
 
-    def memoize(self, key, value):
-        memo[key] = value
-    
+    @classmethod
+    def memoize(cls, key, value):
+        #print 'Memoizing',cls.name, key, u'\t→\t', value
+        cls.memo[key] = value
+        return value
+
+@add_name_stuff
 class CommandWhich(Which):
-    def __init__(self, name):
-        super(self.__class__, self).__init__(name)
-        self.opposite = WordWhich
-        self.test = lambda largest_end_of_prev_command: [largest_end_of_prev_command[:j] for j in range(len(largest_end_of_prev_command), 0, -1)]
+    name = 'command'
 
+    @staticmethod
+    def test(end):
+        return [end[:j] for j in range(len(end), 0, -1)]
+
+@add_name_stuff
 class WordWhich(Which):
-    def __init__(self, name):
-        super(self.__class__, self).__init__(name)
-        self.opposite = CommandWhich
-        self.test = lambda largest_end_of_prev_command: [largest_end_of_prev_word]
+    name = 'word'
 
-whiches = {
-    'command': {
-        'opposite': 'word',
-        'test': lambda largest_end_of_prev_command: [largest_end_of_prev_command[:j] for j in range(len(largest_end_of_prev_command), 0, -1)]
-    },
-    'word': {
-        'opposite': 'command',
-        'test': lambda largest_end_of_prev_word: [largest_end_of_prev_word]
-    }
-}
-for key in whiches:
-    whiches[key].update({
-        'name': key,
-        'memoized': datrie.Trie(string.ascii_lowercase),
-        'memoize': lambda k, v: whiches[key]['memoized'].__setitem__(k, v),
-        'reversed_trie': globals()['reversed_' + key + 's_trie'],
-        'on': globals()[key.upper() + '_ON'],
-        'off': globals()[key.upper() + '_OFF'],
-    })
+    @staticmethod
+    def test(end):
+        return [end]
 
+CommandWhich.opposite = WordWhich
+WordWhich.opposite = CommandWhich
 
-print CommandWhich.memo
-pass
-
-#whiches['word']['memoize'](u'a', '3')
-print whiches['word']['memoized'].__setitem__(u'a', 3)
-print whiches['word']['memoized'].keys()
-exit()
-
-def which_memoize(which, key, value):
-    trie[key] = value
-    print 'Memoizing', key, '=', value
-    return value
 
 # Find all commands that end with a string
-#@memoize
 def ending_with(which, end_of_this, n):
     indent = '>>\t'*n
-    #which = whiches[which]
-    #print
-    print indent+'['+BOLD_ON+which['name']+'s'+BOLD_OFF+'] ending in '+end_of_this+'\t'
-    if end_of_this in which['memoized']:
-        return which.memoize
+    ##print indent+'['+BOLD_ON+which.name+'s'+BOLD_OFF+'] ending in '+end_of_this+'\t'
+    try:
+        shortest_suffix, this = which.memo.iter_prefix_items(end_of_this).next()
+        print end_of_this, ':', shortest_suffix, this
+        return this
+    except StopIteration:
+        pass
+    
+    #for memo in memos:
+
+    #print memos
+    if end_of_this in which.memo:
         #print '(Memoized!    )'
-        return which['memoized'][end_of_this]
+        return which.memo[end_of_this]
     #print '(Not memoized.)',
     #print '~'*14
 
-    completions = which['reversed_trie'].keys(end_of_this)[:92]
+    completions = which.reversed_trie.keys(end_of_this)#[:92]
     completions.sort(key=len)
-    print indent+'Found',len(completions),'completions:', completions
+    ##print indent+'Found',len(completions),'completions:', completions
 
     l = len(end_of_this)
-    if not which['reversed_trie'].has_keys_with_prefix(end_of_this):
-        which['memoized'][end_of_this] = None
-        print indent+'Memoizing None.'
-        return
+    if not which.reversed_trie.has_keys_with_prefix(end_of_this):
+        ##print indent,
+        return which.memoize(end_of_this, None)
 
     for this in completions:
         largest_end_of_prev = this[:-l]
-        end_of_prevs = which['test'](largest_end_of_prev)
+        end_of_prevs = which.test(largest_end_of_prev)
         #print indent+'>'+this+':\t', 'largest_end_of_prev:',largest_end_of_prev,'\tend_of_prevs:',' '.join(end_of_prevs)
         
         # This means this is a command or word by itself
         if not largest_end_of_prev:
-            which['memoized'][end_of_this] = this
-            return which['memoized'][end_of_this]
+            ##print indent,
+            return which.memoize(end_of_this, this)
 
         for end_of_prev in end_of_prevs:
             # m = n - len(str(end_end_of_prev))
-            prev = ending_with(which['opposite'], end_of_prev, 1+n)
+            prev = ending_with(which.opposite, end_of_prev, 1+n)
 
             if prev:
                 #print indent+which['on'] + str(prev) + which['off']
-                which['memoized'][end_of_this] = prev + end_of_this
-                return which['memoized'][end_of_this]
+                ##print indent,
+                return which.memoize(end_of_this, (prev, end_of_this))
 
-    which['memoized'][end_of_this] = None
-    return
-# -------------
+    return which.memoize(end_of_this, None)
 
 
+test_words = ['tire', 'paw', 'eft', 'own', 'art', 'through', 'prefix', 'it']
+test_words = ['apple', 'banana', 'durian', 'egg', 'fruit', 'guava', 'hello', 'iodine',
+ 'loquat', 'nutmeg', 'orange', 'pear', 'raisin', 'sugar', 'time', 'under', 'what']
 
+def main():
+    for word in map(unicode, test_words):
+        result = complete(word,0)
+        print word+'\t', BOLD_ON+UNDERLINE_ON+str(result)+UNDERLINE_OFF+BOLD_OFF
 
-
-
-def commands_ending_with(end_of_this_command):
-    if end_of_this_command in commands_ending_with_memoized:
-        return commands_ending_with_memoized[end_of_this_command]
-    ii = len(end_of_this_command)
-    if not reversed_commands_trie.has_keys_with_prefix(end_of_this_command):
-        commands_ending_with_memoized[end_of_this_command] = None
-        return
-    completions = reversed_commands_trie.keys(end_of_this_command)
-    for this_command in completions:
-        largest_end_of_prev_word = this_command[ii:]
-
-        # This means this_command is a word by itself ("a")
-        if not largest_end_of_prev_word:
-            commands_ending_with_memoized[end_of_this_command] = this_command[::-1]
-            return commands_ending_with_memoized[end_of_this_command]
-
-        for j in range(len(largest_end_of_prev_word), 0, -1):
-            end_of_prev_word = largest_end_of_prev_word[:j]
-            # m = n - len(str(end_of_prev_word))
-            prev_word = words_ending_with(end_of_prev_word)
-            if prev_word:
-                print WORD_ON+str(prev_word)+WORD_OFF
-                commands_ending_with_memoized[end_of_this_command] = prev_word + end_of_this_command[::-1]
-                return commands_ending_with_memoized[end_of_this_command]
-    commands_ending_with_memoized[end_of_this_command] = None
     return
 
-#@memoize
-def words_ending_with(end_of_this_word):
-    if end_of_this_word in words_ending_with_memoized:
-        return words_ending_with_memoized[end_of_this_word]
-    print 'LOOKING FOR WORDS ENDING WITH',end_of_this_word[::-1]
-    if not reversed_words_trie.has_keys_with_prefix(end_of_this_word):
-        words_ending_with_memoized[end_of_this_word] = None
-        return
-
-    iii = len(end_of_this_word)
-    completions = reversed_words_trie.keys(end_of_this_word)
-    for this_word in completions:
-        #print 'FOUND WORD:', this_word[::-1]
-        largest_end_of_prev_command = this_word[iii:]
-
-        # This means this_word is a command by itself
-        if not largest_end_of_prev_command:
-            words_ending_with_memoized[end_of_this_word] = this_word[::-1]
-            return words_ending_with_memoized[end_of_this_word]
-
-        end_of_prev_command = largest_end_of_prev_command#[:j]
-        #m = n - len(str(end_of_prev_command))
-        prev_command = commands_ending_with(end_of_prev_command)
-        if prev_command:
-            print COMMAND_ON+str(prev_command)+COMMAND_OFF
-            words_ending_with_memoized[end_of_this_word] = prev_command + end_of_this_word[::-1]
-            return words_ending_with_memoized[end_of_this_word]
-    words_ending_with_memoized[end_of_this_word] = None
-    return
-
-#test_words = ['tire', 'paw', 'eft', 'own', 'art', 'through', 'prefix', 'it']
-#test_words = ['apple', 'banana', 'durian', 'egg', 'fruit', 'guava', 'hello', 'iodine', 'loquat', 'nutmeg', 'orange', 'pear', 'raisin', 'sugar', 'time', 'under', 'what']
-
-test_words = map(unicode, test_words)
-for word in test_words:
-    result = complete(word,0)
-    print word, BOLD_ON+UNDERLINE_ON+str(result)+UNDERLINE_OFF+BOLD_OFF
-
-print commands_ending_with_memoized
-print words_ending_with_memoized
-
-exit()
+memoized = {}
 
 def segment(word):
     if word in commands:
@@ -280,7 +207,12 @@ def segment(word):
                 return memoized[word]
         memoized[word] = None
 
-for word in words:
-    seg = segment(word)
-    if seg is not None:
-        print word, seg
+if 0:
+    for word in words:
+        seg = segment(word)
+        if seg is not None:
+            print word, seg
+
+
+if __name__ == '__main__':
+    main()
